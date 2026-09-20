e12cf6418348fbd194a6ce9612b65e6e0bfb4ced
nopin01 <delpinjohnanasco@gmail.com>
2026-09-21 01:32:37 +0800

    Update API_URL to new endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // Point this at your backend.
 // Local testing:  http://localhost:5000
 // After Render:   https://your-api.onrender.com
-   const API_URL = "https://login-system-3-d45l.onrender.com";
+const API_URL = "https://login-system-3-oui7.onrender.com";
